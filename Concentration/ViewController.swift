8636e88769ca33f2618b64146627d1e960623d5d
Jian Guo <withgj@gmail.com>
2018-08-23 10:36:32 -0500

    first IOS app

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        let cardNumber = cardButtons.index(of: sender)
+        print("cardNumber = \(cardNumber)")
+        //flipCard(withEmoji: "👻", on: sender)
     }
-
-
+    
+    func flipCard(withEmoji emoji:String, on button: UIButton) {
+        print("flip card with emoji: \(emoji)")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
